55a3d9c42fae6477d12a577c549498cdd4d130e4
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-07-28 14:23:36 -0700

    finished

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(newFirstName = 'Anonymous', newLastName = 'Person', newAge, married = false)  {
+  
+    return {
+    firstName: newFirstName,
+    lastName: newLastName,
+    age: newAge,
+    married: married,
+
+    goingOn: function() {
+      return this.age + 1;
+    },
+    ageUp: function() {
+      return this.age++
+    },
+    getFullName: function() {
+      return `${this.firstName} ${this.lastName}`
+    },
+    marry: function(spouse) {
+      this.married = true;
+      spouse.married = true;
+      this.spouseName = spouse.getFullName();
+      spouse.spouseName = this.getFullName()
+    },
+    divorce: function(spouse) {
+      this.married = false;
+      spouse.married = false;
+      delete this.spouseName;
+      delete spouse.spouseName
+    }
+    }
+  
+  
+}
+
 
 
 
